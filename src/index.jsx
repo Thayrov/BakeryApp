7b4626af3f2f2ce58308cd428b15f1f17d92c8a1
thayrov <thayrovg@gmail.com>
2023-02-06 10:03:14 -0600

    add babel parser config to resolve conflict with esLint

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,11 @@ import { Text, View } from 'react-native';
 
 import { styles } from './styles';
 
-export default function App() {
+const App = () => {
     return (
         <View style={styles.container}>
             <Text>Open up App.js to start working on your app!</Text>
         </View>
     );
-}
+};
+export default App;
