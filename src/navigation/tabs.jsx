9682b6771d042de0ca7e4d129165810c6f90cf4a
thayrov <thayrovg@gmail.com>
2023-02-07 10:58:35 -0600

    add custom icons to tabs

--- src/navigation/tabs.jsx
+++ src/navigation/tabs.jsx
@@ -1,5 +1,6 @@
 import CartNavigator from './cart';
 import { Dimensions } from 'react-native';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import OrdersNavigator from './orders';
 import ShopNavigator from './shop';
 import { THEME } from '../constants/theme';
@@ -17,7 +18,7 @@ const TabsNavigator = () => {
                 headerShown: false,
                 tabBarLabelStyle: {
                     fontFamily: 'Rancho-Regular',
-                    fontSize: ((height * width) / 1000) * 0.08,
+                    fontSize: ((height * width) / 1000) * 0.045,
                     color: THEME.colors.TextColor,
                 },
                 tabBarActiveTintColor: THEME.colors.SecondaryColor,
@@ -26,17 +27,51 @@ const TabsNavigator = () => {
             <BottomTab.Screen
                 name="ShopTab"
                 component={ShopNavigator}
-                options={{ title: 'Shop' }}
+                options={{
+                    title: 'Shop',
+                    tabBarIcon: ({ focused }) => (
+                        <MaterialCommunityIcons
+                            name={focused ? 'bread-slice' : 'bread-slice-outline'}
+                            size={20}
+                            color={
+                                focused ? THEME.colors.SecondaryColor : THEME.colors.NeutralColor
+                            }
+                        />
+                    ),
+                }}
             />
             <BottomTab.Screen
                 name="OrdersTab"
                 component={OrdersNavigator}
-                options={{ title: 'Orders' }}
+                options={{
+                    title: 'Orders',
+
+                    tabBarIcon: ({ focused }) => (
+                        <MaterialCommunityIcons
+                            name={focused ? 'basket' : 'basket-outline'}
+                            size={20}
+                            color={
+                                focused ? THEME.colors.SecondaryColor : THEME.colors.NeutralColor
+                            }
+                        />
+                    ),
+                }}
             />
             <BottomTab.Screen
                 name="CartTab"
                 component={CartNavigator}
-                options={{ title: 'Cart' }}
+                options={{
+                    title: 'Cart',
+                    tabBarIcon: ({ focused }) => (
+                        <MaterialCommunityIcons
+                            name={focused ? 'cart' : 'cart-outline'}
+                            size={20}
+                            color={
+                                focused ? THEME.colors.SecondaryColor : THEME.colors.NeutralColor
+                            }
+                        />
+                    ),
+                }}
             />
         </BottomTab.Navigator>
     );
