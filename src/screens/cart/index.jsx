239b98d2969baf6b3f22ed4cd2a51ef43975bb50
thayrov <thayrovg@gmail.com>
2023-03-04 19:14:58 -0600

    define logic for confirming and sending orders to DB

--- src/screens/cart/index.jsx
+++ src/screens/cart/index.jsx
@@ -1,13 +1,20 @@
 import { FlatList, Text, TouchableOpacity, View } from 'react-native';
+import { confirmOrder, removeFromCart } from '../../store/actions/cart.action';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { CartItem } from '../../components';
 import styles from './styles';
-import { useSelector } from 'react-redux';
 
 const Cart = ({ navigation }) => {
+    const dispatch = useDispatch();
     const cart = useSelector((state) => state.cart.items);
     const total = useSelector((state) => state.cart.total);
-    const onDelete = (id) => {};
+    const onDelete = (id) => {
+        dispatch(removeFromCart(id));
+    };
+    const sendOrder = () => {
+        dispatch(confirmOrder(cart, total));
+    };
     const renderItem = ({ item }) => <CartItem item={item} onDelete={onDelete} />;
     const keyExtractor = (item) => item.id.toString();
     const Header = () =>
@@ -21,7 +28,7 @@ const Cart = ({ navigation }) => {
     const Footer = () =>
         cart.length > 0 && (
             <View style={styles.footer}>
-                <TouchableOpacity style={styles.confirm} onPress={() => null}>
+                <TouchableOpacity style={styles.confirm} onPress={sendOrder}>
                     <Text style={styles.confirmText}>Confirm</Text>
                     <View style={styles.totalContainer}>
                         <Text style={styles.totalText}>Total: </Text>
--- src/store/actions/cart.action.js
+++ src/store/actions/cart.action.js
@@ -1,3 +1,4 @@
+import { REALTIME_DB_URL } from '../../constants/firebase/index';
 import { cartTypes } from '../types';
 
 const { ADD_TO_CART, REMOVE_FROM_CART, CONFIRM_ORDER } = cartTypes;
@@ -12,7 +13,30 @@ export const removeFromCart = (id) => ({
     id,
 });
 
-export const confirmOrder = (order) => ({
-    type: CONFIRM_ORDER,
-    order,
-});
+export const confirmOrder = (items, total) => {
+    return async (dispatch) => {
+        try {
+            const response = await fetch(`${REALTIME_DB_URL}/orders.json`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    date: Date.now(),
+                    items,
+                    total,
+                }),
+            });
+            const result = await response.json();
+            dispatch({
+                type: CONFIRM_ORDER,
+                result,
+            });
+        } catch (error) {
+            dispatch({
+                type: CONFIRM_ORDER,
+                error,
+            });
+        }
+    };
+};
